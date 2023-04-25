ad66d1c85cde2042c2513708f222bb7963625e1e
Ben Purinton <ben.purinton@gmail.com>
2023-04-25 15:06:38 -0800

    rock rendered in views

--- app.rb
+++ app.rb
@@ -29,5 +29,10 @@ get('/rock') do
   # redirect("https://www.wikipedia.org")
 
   # or we can render an html page from the root
-  send_file("rock.html")
+  # send_file("rock.html")
+
+  # but even better, we can make a views folder
+  # and render a new type of file from there
+  # we call the file "views/rock.erb"
+  erb(:rock)
 end
