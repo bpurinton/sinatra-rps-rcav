f3764a95f75efc43ffe68242d8b531ea13e04baa
Ben Purinton <ben.purinton@gmail.com>
2023-04-25 15:01:08 -0800

    next steps wip

--- app.rb
+++ app.rb
@@ -22,3 +22,12 @@ get('/') do
   # we can pass HTML
   "<h1>Hello, world!</h1>"
 end
+
+# we can define another route to our rock page
+get '/rock' do
+  # we can simply redirect somewhere
+  # redirect("https://www.wikipedia.org")
+
+  # or we can render an html page from the root
+  send_file("rock.html")
+end
