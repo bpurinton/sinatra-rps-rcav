cac14205ff40d0044d36fbca7903761db2f5affd
Ben Purinton <ben.purinton@gmail.com>
2023-04-26 16:18:25 -0800

    moved everything out of app

--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
-require 'bundler/setup'
+require './config/environment'
 run Sinatra::Application
--- config/environment.rb
+++ config/environment.rb
@@ -1,27 +1,23 @@
 require './app'
 
-# `settings` is defined in sinatra we want to also 
-# allow the live preview to reload on changes if we 
-# are in development mode
-if settings.development?
+configure do
+  # To open .html.erb files, need to register them
+  Tilt.register Tilt::ERBTemplate, 'html.erb'
+  
+  # alternative to setting the layout in every erb() call
+  # we can set the default layout name to use and sinatra 
+  # will look for that file
+  set(:erb, :layout => :application_layout)
+end
+
+configure :development do
   require "sinatra/reloader"
 
   # we would also like a nicer error page in development
   require 'better_errors'
   require 'binding_of_caller'
-  use(BetterErrors::Middleware)
+  
+  # need this configure for better errors
+  use BetterErrors::Middleware
   BetterErrors.application_root = __dir__
 end
-
-# we use the `set` method with the :port argument to 
-# set what port our live preview shows when we run 
-# `ruby app.rb`
-set(:port, 3000)
-
-# To open .html.erb files, need to register them
-Tilt.register(Tilt::ERBTemplate, 'html.erb')
-
-# alternative to setting the layout in every erb() call
-# we can set the default layout name to use and sinatra 
-# will look for that file
-set(:erb, :layout => :application_layout)
